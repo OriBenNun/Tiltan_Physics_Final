fca3f70c385f688e2b8e414db5a53eb794229478
Ori Ben nun <57292081+OriBenNun@users.noreply.github.com>
2024-02-12 17:08:42 +0200

    Added some useless buggy bounciness logic for the class task

--- Assets/Scripts/CannonBall.cs
+++ Assets/Scripts/CannonBall.cs
@@ -2,26 +2,62 @@ using UnityEngine;
 
 public class CannonBall : MonoBehaviour
 {
-    private const float Gravity = 9.81f;
-
+    [SerializeField] private float gravity = 9.81f;
     [SerializeField] private float mass = 10;
-    [SerializeField] private float startingForce;
-
-    private Vector3 _force;
+    [SerializeField] private float bounciness = 0.7f;
+    [SerializeField] private float jumpForce = 5;
     
-    // Start is called before the first frame update
-    void Start()
+    private Vector3 _force;
+    private bool _isOnGround;
+
+    private void Update()
     {
-        _force = Vector3.up * (mass * startingForce);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_isOnGround)
+        {
+            return;
+        }
+        
+        _force += Vector3.down * (mass * gravity);
+        
+        transform.Translate(Time.fixedDeltaTime * _force);
+    }
+    
+    private void Jump()
+    {
+        if (_isOnGround)
+        {
+            _isOnGround = false;
+        }
+        
+        Debug.Log($"Jumped! Force: {_force}");
+        _force = Vector3.up * (mass * jumpForce);
         transform.Translate(_force);
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    // Bounciness formula Vf = - (E * Vi)
+    // Where Vf is Final Velocity, E is bounciness factor [between 0 to 1] and Vi is Initial Velocity
+    private void OnCollisionEnter(Collision other)
     {
-        // _velocity -= Gravity * Time.fixedDeltaTime;
-        _force += Vector3.down * (mass * Gravity);
-        
-        transform.Translate(_force * Time.fixedDeltaTime);
+        _force = -(bounciness * _force);
+    }
+
+    private void OnCollisionStay(Collision other)
+    {
+        Debug.Log($"HERE");
+        if (transform.position.y < 0.4f)
+        {
+            _isOnGround = true;
+            var position = transform.position;
+            position.y = 0.5f;
+            transform.position = position;
+        }
     }
 }
