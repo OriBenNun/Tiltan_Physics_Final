7e265f6bc240fbba92fcfd203d076cae8b055942
BmanStudio <57292081+BmanStudio@users.noreply.github.com>
2024-04-20 22:09:30 +0300

    minor changes

--- Assets/Scripts/PhysicsSystem/SpringConfigSo.cs
+++ Assets/Scripts/PhysicsSystem/SpringConfigSo.cs
@@ -5,7 +5,6 @@ namespace PhysicsSystem
     [CreateAssetMenu(menuName = "Springs", fileName = "New_SpringConfigSo", order = 0)]
     public class SpringConfigSo : ScriptableObject
     {
-        public string springName;
         public float theKFactor = 1f;
         public float minimumDisplacement;
         public float maximumDisplacement = 4f;
